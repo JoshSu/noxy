c4b09ec143029fb3c67319b244fb7799ea8ec44d
burton <burton@spinn3r.com>
2015-11-09 14:07:47 -0800

    update version dependencies.. working on an admin servlet for showing noxy IPs and config.

---
+++ noxy-reverse/src/main/java/com/spinn3r/noxy/reverse/admin/servlets/ServersAdminServlet.java
@@ -0,0 +1,22 @@
+package com.spinn3r.noxy.reverse.admin.servlets;
+
+import org.eclipse.jetty.servlet.DefaultServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class ServersAdminServlet extends DefaultServlet {
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
